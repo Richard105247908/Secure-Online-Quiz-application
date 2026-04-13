a392fbd7a4f3d9cb8d5a131050c61a274b785ad8
Richard <rj.n@live.com>
2026-04-13 13:37:51 +0100

    used Integer and Question for hashmap questions , then add an int nextid for next question

--- src/main/java/com/example/Secure/Online/Quiz/Application/service/QuestionsService.java
+++ src/main/java/com/example/Secure/Online/Quiz/Application/service/QuestionsService.java
@@ -1,8 +1,15 @@
 package com.example.Secure.Online.Quiz.Application.service;
 
+import com.example.Secure.Online.Quiz.Application.model.Question;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class QuestionsService {
+
+private HashMap<Integer, Question>questions=new HashMap<>();
+private int nextId = 1;
+
 
 }
