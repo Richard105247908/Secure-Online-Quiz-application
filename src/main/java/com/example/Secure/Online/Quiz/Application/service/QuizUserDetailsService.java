beee9e85082363f97c6470a69d779fb2d27d0a38
Richard <rj.n@live.com>
2026-04-13 12:05:51 +0100

    added methods for quiz user service and also password

--- src/main/java/com/example/Secure/Online/Quiz/Application/service/QuizUserDetailsService.java
+++ src/main/java/com/example/Secure/Online/Quiz/Application/service/QuizUserDetailsService.java
@@ -1,4 +1,30 @@
 package com.example.Secure.Online.Quiz.Application.service;
 
-public class QuizUserDetailsService {
+import com.example.Secure.Online.Quiz.Application.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class QuizUserDetailsService implements UserDetailsService {
+
+   private final HashMap<String, User>users= new HashMap<>();
+   private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+   @Override
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+       User user = users.get(username);
+       if (user == null) {
+           throw new UsernameNotFoundException("User not found");
+       }
+
+       return org.springframework.security.core.userdetails.
+   }
+
 }
