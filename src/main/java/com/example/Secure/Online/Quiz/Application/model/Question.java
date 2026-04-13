914d64408bb040d98bc829835386d152bdd5989a
Richard <rj.n@live.com>
2026-04-13 11:27:32 +0100

    question class created with the attributes

--- src/main/java/com/example/Secure/Online/Quiz/Application/model/Question.java
+++ src/main/java/com/example/Secure/Online/Quiz/Application/model/Question.java
@@ -1,4 +1,14 @@
 package com.example.Secure.Online.Quiz.Application.model;
 
+import java.util.ArrayList;
+
 public class Question {
+
+    private int id;
+    private String questionText;
+    private ArrayList<String> options;
+    private String correctAnswers;
+
+    
+
 }
