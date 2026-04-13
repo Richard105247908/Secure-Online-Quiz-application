09cb6d4047e3ff5ecb536013bb7f68b437f1c73c
Richard <rj.n@live.com>
2026-04-13 10:46:35 +0100

    spring initialiser is added

---
+++ src/main/java/com/example/Secure/Online/Quiz/Application/SecureOnlineQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.Secure.Online.Quiz.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureOnlineQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureOnlineQuizApplication.class, args);
+	}
+
+}
