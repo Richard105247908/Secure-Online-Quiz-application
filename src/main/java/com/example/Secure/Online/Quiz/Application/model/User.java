3667542f772e5d654196fdd176dd7a68d8a8848b
Richard <rj.n@live.com>
2026-04-13 11:13:52 +0100

    user class added to model

---
+++ src/main/java/com/example/Secure/Online/Quiz/Application/model/User.java
@@ -0,0 +1,10 @@
+package com.example.Secure.Online.Quiz.Application.model;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    
+}
